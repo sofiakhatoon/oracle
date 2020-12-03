4bc2c6d6bcef292228fea492bd735a43c6320cbc
sofiakhatoon <44379240+sofiakhatoon@users.noreply.github.com>
2020-12-03 14:05:36 -0600

    Add files via upload

---
+++ Untitled3.ps1
@@ -0,0 +1,22 @@
+﻿ 
+    
+
+ Add-Type -Path "C:\Oracle\product\12.2.0\client_1\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+$username ='xx'
+$password = "yy"
+$datasource = "test"
+$query = "select cde_x from t_xy "
+$connectionString = 'User Id=' + $username + ';Password=' + $password + ';Data Source=' + $datasource
+$connection = New-Object Oracle.ManagedDataAccess.Client.OracleConnection($connectionString)
+$connection.open()
+$command=$connection.CreateCommand()
+$command.CommandText=$query
+$reader=$command.ExecuteReader()
+while ($reader.Read()) {
+ $reader.GetDecimal(0)
+ $reader.GetDecimal(1)
+ $reader.GetString(2)
+ }
+
+ $connection.Close()
+
